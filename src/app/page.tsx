8425abd505e9bbe35e6e740b940eb047172af27e
NeemaMwende <gracepete12@gmail.com>
2025-02-19 18:16:42 +0300

    updates: sessions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
-"use client"
-import { login } from "@/lib/actions/auth";
+"use server"
+import { auth } from "@/auth";
+import { SignInButton } from "./components/sign-in-button";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+ 
+  if (session?.user) {
+    return ( 
+      <div>
+        {" "}
+        <h1>Signin Successful</h1>
+        <p>User signed in with name: {session.user.name}</p>
+         <p>User signed in with email: {session.user.email}</p>
+         {session.user.image && (
+        <Image 
+          src={session.user.image} 
+          alt="Profile pic" 
+          width={80} 
+          height={80} 
+          style={{borderRadius: "50%"}}
+        />
+      )}
+
+      </div>
+    ) 
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p> You are not Signed In</p>
-      <button onClick={() => login()}>Sign In with Github</button>
+        <SignInButton/>
     </div>
   );
 }
