e2c6eafce3ea0c739f50a1939c35edd2d37d60ed
NeemaMwende <gracepete12@gmail.com>
2025-02-19 15:48:33 +0300

    environment variables and auth set up

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+
+export const { auth, handlers, signIn, signOut } = NextAuth ({
+  providers: [
+    GitHub
+  ],
+});
+
+// export default NextAuth(authOptions);
