8fe38634570e4064d21255d4bd15f18f08139475
NeemaMwende <gracepete12@gmail.com>
2025-02-19 23:20:55 +0300

    signout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,17 @@
 "use server"
 import { auth } from "@/auth";
 import { SignInButton } from "./components/sign-in-button";
-import Image from "next/image";
+import Link from "next/link";
+import { SignOutButton } from "./components/sign-out-button";
 
 export default async function Home() {
   const session = await auth();
  
   if (session?.user) {
     return ( 
-      <div>
-        {" "}
-        <h1>Signin Successful</h1>
-        <p>User signed in with name: {session.user.name}</p>
-         <p>User signed in with email: {session.user.email}</p>
-         {session.user.image && (
-        <Image 
-          src={session.user.image} 
-          alt="Profile pic" 
-          width={80} 
-          height={80} 
-          style={{borderRadius: "50%"}}
-        />
-      )}
-
+      <div className=" items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <Link href="/user-info">User Info</Link>
+        <SignOutButton/>
       </div>
     ) 
   }
